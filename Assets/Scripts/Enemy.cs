68ad64098c11de31b8730219d2469801bb96423b
Sebight <59232422+Sebight@users.noreply.github.com>
2022-03-02 22:16:47 +0100

    enemies movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,30 +2,62 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public enum EnemyType
 {
     Normal,
-    Stealth, 
+    Stealth,
     Boss
 }
 
-public class Enemy
+public class Enemy : MonoBehaviour
 {
+    public GameManager gameManager;
+
+    public Transform pathWaypoints;
+
     public int health;
     public int damage;
     public int speed;
     public EnemyType type;
+
+    private int currentWaypoint = 0;
+
     public virtual void Die()
     {
     }
 
-    public void TakeDamage(int damage)
+    public virtual void Initialize(GameManager gameManager)
+    {
+        this.gameManager = gameManager;
+    }
+
+    public virtual void Navigate()
+    {
+        if (pathWaypoints == null) pathWaypoints = gameManager.GetPathWaypoints();
+        transform.position = Vector3.MoveTowards(transform.position, pathWaypoints.GetChild(currentWaypoint).position, speed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, pathWaypoints.GetChild(currentWaypoint).position) < 0.1f)
+        {
+            currentWaypoint++;
+            if (currentWaypoint >= pathWaypoints.childCount)
+            {
+                FinishPath();
+            }
+        }
+    }
+
+    public virtual void TakeDamage(int damage)
     {
         health -= damage;
         if (health <= 0)
         {
             Die();
         }
+    }
+
+    public virtual void FinishPath()
+    {
+        gameManager.RemoveEnemy(this);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,41 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public List<Tower> towersPlaced = new List<Tower>();
+    public List<GameObject> enemiesPrefabs = new List<GameObject>();
     public List<Enemy> enemies = new List<Enemy>();
+
+    public Transform pathWaypoints;
     
     //Health left for the player
     public int health = 100;
+
+    public Transform GetPathWaypoints() => pathWaypoints;
+
+    public void RemoveEnemy(Enemy e)
+    {
+        enemies.Remove(e);
+        Destroy(e.gameObject);
+    }
+
+    public void SpawnEnemy()
+    {
+        GameObject enemy = Instantiate(enemiesPrefabs[UnityEngine.Random.Range(0, enemiesPrefabs.Count)], pathWaypoints.GetChild(0).position, Quaternion.identity);
+        Enemy e = enemy.GetComponent<Enemy>();
+        e.Initialize(this);
+        enemies.Add(e);
+    }
+
+    public void Start()
+    {
+        InvokeRepeating(nameof(SpawnEnemy), 0.1f, 0.5f);
+    }
+    
+    public void Update()
+    {
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].Navigate();
+        }
+    }
+    
 }
